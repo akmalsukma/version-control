f45ace3e6e68f5f8f088c1203456a3cd2e19c648
Akmal Permata Sukma <55771373+akmalsukma@users.noreply.github.com>
2020-04-08 14:34:05 +0700

    Menghapus alamat rumah

--- aboutme.php
+++ aboutme.php
@@ -2,7 +2,6 @@
     echo "Nama : Akmal Permata Sukma <br>";
     echo "Kelas : XI RPL 1 <br>";
     echo "Asal Sekolah : SMK Taruna Bhakti <br>";
-    echo "Alamat Rumah : Jln. Jatijajar 1 Gg al-ghofur 3 No 103 Rt 06 Rw 01 <br> ";
     echo "No Hp : 087788860479";
     echo "Email : akmal.awe2@gmail.com"
  
