32baad6b1d9d9ae064ac4a97fbe2c9bc460ca89d
Akmal Permata Sukma <55771373+akmalsukma@users.noreply.github.com>
2020-04-08 14:33:02 +0700

    Menambahkan no hp dan email

--- aboutme.php
+++ aboutme.php
@@ -3,5 +3,7 @@
     echo "Kelas : XI RPL 1 <br>";
     echo "Asal Sekolah : SMK Taruna Bhakti <br>";
     echo "Alamat Rumah : Jln. Jatijajar 1 Gg al-ghofur 3 No 103 Rt 06 Rw 01 <br> ";
+    echo "No Hp : 087788860479";
+    echo "Email : akmal.awe2@gmail.com"
  
  
