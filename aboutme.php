eb651d18fb58d8348693694a5cf8f87720dda751
Akmal Permata Sukma <55771373+akmalsukma@users.noreply.github.com>
2020-04-08 14:31:04 +0700

    Membuat About me

---
+++ aboutme.php
@@ -0,0 +1,7 @@
+<?php 
+    echo "Nama : Akmal Permata Sukma <br>";
+    echo "Kelas : XI RPL 1 <br>";
+    echo "Asal Sekolah : SMK Taruna Bhakti <br>";
+    echo "Alamat Rumah : Jln. Jatijajar 1 Gg al-ghofur 3 No 103 Rt 06 Rw 01 <br> ";
+ 
+ 
